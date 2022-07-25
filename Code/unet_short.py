42787947c2973c57d0f496c6397b3a6e310c9f53
Adrian.Buchwald <Adrian.Buchwald@uni-potsdam.de>
2022-07-25 13:24:10 +0200

    Now with GPU-Support and some first predictions! Model is pretty shallow though, since the gpu overfloats pretty often caused by the 3D Convolutions.

--- Code/unet_short.py
+++ Code/unet_short.py
@@ -20,11 +20,11 @@ class UNet(nn.Module):
         self.upconv2 = nn.ConvTranspose3d(
             features * 4, features * 2, kernel_size=2, stride=1
         )
-        self.decoder2 = UNet._block((features * 2) * 2, features * 2, name="dec2")
+        self.decoder2 = UNet._block((features * 2), features * 2, name="dec2")
         self.upconv1 = nn.ConvTranspose3d(
             features * 2, features, kernel_size=2, stride=1
         )
-        self.decoder1 = UNet._block(features * 2, features, name="dec1")
+        self.decoder1 = UNet._block(features, features, name="dec1")
 
         self.conv = nn.Conv3d(
             in_channels=features, out_channels=out_channels, kernel_size=1
@@ -37,10 +37,8 @@ class UNet(nn.Module):
         bottleneck = self.bottleneck(self.pool2(enc2))
         
         dec2 = self.upconv2(bottleneck)
-        dec2 = torch.cat((dec2, enc2), dim=1)
         dec2 = self.decoder2(dec2)
         dec1 = self.upconv1(dec2)
-        dec1 = torch.cat((dec1, enc1), dim=1)
         dec1 = self.decoder1(dec1)
         return torch.sigmoid(self.conv(dec1))
 
@@ -61,18 +59,18 @@ class UNet(nn.Module):
                     ),
                     (name + "norm1", nn.BatchNorm3d(num_features=features)),
                     (name + "relu1", nn.ReLU(inplace=True)),
-                    (
-                        name + "conv2",
-                        nn.Conv3d(
-                            in_channels=features,
-                            out_channels=features,
-                            kernel_size=3,
-                            padding=1,
-                            bias=False,
-                        ),
-                    ),
-                    (name + "norm2", nn.BatchNorm3d(num_features=features)),
-                    (name + "relu2", nn.ReLU(inplace=True)),
+                    #(
+                    #    name + "conv2",
+                    #    nn.Conv3d(
+                    #        in_channels=features,
+                    #        out_channels=features,
+                    #        kernel_size=3,
+                    #        padding=1,
+                    #        bias=False,
+                    #    ),
+                    #),
+                    #(name + "norm2", nn.BatchNorm3d(num_features=features)),
+                    #(name + "relu2", nn.ReLU(inplace=True)),
                 ]
             )
         )
