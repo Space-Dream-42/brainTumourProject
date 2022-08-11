6613968746e9b2a17a36fbfe03e6492db59493c8
till2 <89709351+till2@users.noreply.github.com>
2022-08-11 16:41:51 +0200

    add test_loss to training loop

--- Code/train.py
+++ Code/train.py
@@ -1,36 +1,75 @@
 import torch
 import torch.nn as nn
+import numpy as np
 from custom_losses import get_loss
 from dataset_utils import split_cube, slice_cube
 
-def train_model(model, optimizer, loss_fn, epochs, device, has_minicubes, train_iter, steps_per_epoch = 400):
-    if has_minicubes:
+def train_model(model, optimizer, loss_fn, epochs, device, train_3d, train_iter, test_iter, compute_test_loss, batches_per_epoch=400):
+    num_test_samples = 1 # 1 is only for testing, should be 84
+
+    if train_3d:
         save_rate = 200
+        steps_per_epoch = batches_per_epoch * 4     # multiply by minicube batches per batch
+        test_steps = num_test_samples * 4              
     else:
         save_rate = 1600
+        steps_per_epoch = batches_per_epoch * 160   # multiply by slices per batch
+        test_steps = num_test_samples * 160
+
     # training settings
-    losses = []
     model.train()
+    train_losses = []
+    test_losses = [] if compute_test_loss else None
+
     # training loop
     for epoch in range(epochs):
-        for step in range(steps_per_epoch):
-            if has_minicubes: #check if model is 3D
+        epoch_train_losses = []
+
+        for step in range(steps_per_epoch):  
+            # 3D model
+            if train_3d:
                 if step % 4 == 0:
-                    # Get a new minicube batch
-                    batch = split_cube(train_iter.next())
+                    batch = split_cube(train_iter.next()) # Get a new batch of 3D minicubes
+            
+            # 2D model
             else:
-                if step % 160 == 0: #check if model is 2D
-                    batch = slice_cube(train_iter.next())
-            loss = get_loss(model, loss_fn, has_minicubes, step, device, batch)
-            losses.append(loss)
+                if step % 160 == 0:
+                    batch = slice_cube(train_iter.next()) # Get a new batch of 2D slices
+
+            loss = get_loss(model, loss_fn, train_3d, step, device, batch)
+            epoch_train_losses.append(loss.detach().cpu().numpy())
 
             if step % save_rate == 0:
                 print(f'epoch {epoch}: step {step:3d}: loss={loss:3.3f}')
-                path = f'../Weights/weights_epoch{epoch}_step{step}_loss{loss:3.3f}.h5'
+                path = f'../Weights/{model.__class__.__name__}_epoch{epoch}_step{step}_loss{loss:3.3f}.h5'
                 torch.save(model.state_dict(), path)
 
             # backprop loss
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-    return losses
+        
+        # calculate mean training loss of epoch
+        train_losses.append(np.mean(epoch_train_losses)) 
+
+        # calculate mean test loss of epoch
+        if compute_test_loss:
+            epoch_test_losses = []
+
+            for step in range(test_steps):
+                # 3D model
+                if train_3d:
+                    if step % 4 == 0:
+                        batch = split_cube(test_iter.next()) # Get a new batch of 3D minicubes
+            
+                # 2D model
+                else:
+                    if step % 160 == 0:
+                        batch = slice_cube(test_iter.next()) # Get a new batch of 2D slices
+                
+                loss = get_loss(model, loss_fn, train_3d, step, device, batch)
+                epoch_test_losses.append(loss.detach().cpu().numpy())
+
+            test_losses.append(np.mean(epoch_test_losses)) 
+
+    return train_losses, test_losses
